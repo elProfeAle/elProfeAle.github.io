2570a0708e9583c6a3358755ef2600252cc0cdfb
elProfeAle <37826866@ivt.edu.ar>
2024-05-07 15:27:43 -0300

    nueva estructura

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Copiar al portapapeles
+
+let boton = document.querySelector('.copiado')
+let icon = document.querySelector('#cambiar')
+let code = document.querySelector('pre');
+boton.onclick = function() {
+    navigator.clipboard.writeText(code.textContent);
+
+    icon.classList.remove('bi-clipboard')
+    icon.classList.add('bi-clipboard-check')
+
+}
